64b069c2368b0dbd472e7073b5ac5f9e027d8d30
Mradul999 <demorgans007@gmail.com>
2024-07-12 22:15:52 +0530

    create signin and signup UI

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,10 +1,49 @@
-import React from 'react'
+import React from "react";
+import "../Components/bg.css";
+import { NavLink } from "react-router-dom";
 
 export default function SignUp() {
+  const submitHandler = (e) => {
+    e.preventDefault();
+  };
   return (
-    <div>
-        signup page
-      
+    <div className="h-screen  bg-slate-800 text-white overflow-x-hidden px-3  ">
+      <div className="max-w-[400px] signup mx-auto mt-[9rem] px-3 sm:px-8 py-16  bg-gray-600 rounded-lg  ">
+        <h1 className="text-center text-[2rem] font-semibold mb-6">SignUp</h1>
+        <form onSubmit={submitHandler} className="flex flex-col gap-6">
+          <label className="flex flex-col" htmlFor="username">
+            Enter username
+            <input placeholder="Mradul Verma"
+              className="py-2 rounded-lg text-[13px] pl-2 bg-slate-700 focus:outline-none focus:border-[0.2rem] focus:border-sky-400 "
+              name="username"
+              type="text"
+            />
+          </label>
+          <label className="flex flex-col" htmlFor="username">
+            Enter email
+            <input placeholder="mradul@gmail.com"
+              className="py-2 rounded-lg text-[13px] pl-2 bg-slate-700 focus:outline-none focus:border-[0.2rem] focus:border-sky-400 "
+              name="username"
+              type="text"
+            />
+          </label>
+          <label className="flex flex-col" htmlFor="username">
+            Enter Password
+            <input placeholder="****"
+              className="py-2 rounded-lg text-[13px] pl-2 bg-slate-700 focus:outline-none focus:border-[0.2rem] focus:border-sky-400 "
+              name="username"
+              type="text"
+            />
+          </label>
+          <p>
+            Already regsitered?{" "}
+            <NavLink to="/signin">
+              {" "}
+              <button className="text-sky-500 font-semibold"> Login</button>
+            </NavLink>{" "}
+          </p>
+        </form>
+      </div>
     </div>
-  )
+  );
 }
--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,10 +1,42 @@
-import React from 'react'
-
+import React from "react";
+import "../Components/bg.css";
+import { NavLink } from "react-router-dom";
 export default function Signin() {
+  const submitHandler = (e) => {
+    e.preventDefault();
+  };
   return (
-    <div>
-        signin page
-      
+    <div className="h-screen  bg-slate-800 text-white overflow-x-hidden px-3  ">
+      <div className="max-w-[400px] signup mx-auto mt-[9rem] px-3 sm:px-8 py-16  bg-gray-600 rounded-lg  ">
+        <h1 className="text-center text-[2rem] font-semibold mb-6">Sign In</h1>
+        <form onSubmit={submitHandler} className="flex flex-col gap-6">
+        
+          <label className="flex flex-col" htmlFor="username">
+            Enter  email
+            <input
+              placeholder="mradul@gmail.com"
+              className="py-2 rounded-lg text-[13px] pl-2 bg-slate-700 focus:outline-none focus:border-[0.2rem] focus:border-sky-400 "
+              name="username"
+              type="text"
+            />
+          </label>
+          <label className="flex flex-col" htmlFor="username">
+            Enter Password
+            <input placeholder="****"
+              className="py-2 rounded-lg text-[13px] pl-2 bg-slate-700 focus:outline-none focus:border-[0.2rem] focus:border-sky-400 "
+              name="username"
+              type="text"
+            />
+          </label>
+          <p>
+            Not regsitered yet?{" "}
+            <NavLink to="/signup">
+              {" "}
+              <button className="text-sky-500 font-semibold"> Signup</button>
+            </NavLink>{" "}
+          </p>
+        </form>
+      </div>
     </div>
-  )
+  );
 }
