f1022880c74ab6edd23f88281481873da2e40e5a
Mradul999 <demorgans007@gmail.com>
2024-07-22 17:47:53 +0530

    commit

--- api/index.js
+++ api/index.js
@@ -1,16 +1,22 @@
 import mongoose from "mongoose";
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 
 import authRoute from "./Routes/auth.js";
 import userRoute from "./Routes/user.js";
 import postRoute from "./Routes/post.js";
-import commentroute from "./Routes/comment.js"
-import suggestionroute from "./Routes/suggestion.js"
+import commentroute from "./Routes/comment.js";
+import suggestionroute from "./Routes/suggestion.js";
 import cookieParser from "cookie-parser";
 
 dotenv.config();
 const app = express();
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, "/client/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.use(express.json());
 app.use(cookieParser());
@@ -31,6 +37,5 @@ app.listen(process.env.PORT, () => {
 app.use("/api/auth", authRoute);
 app.use("/api/user", userRoute);
 app.use("/api/post", postRoute);
-app.use("/api/comment",commentroute);
-app.use("/api/suggestion",suggestionroute)
-
+app.use("/api/comment", commentroute);
+app.use("/api/suggestion", suggestionroute);
